11443ab6f22f17de32b9ee617dd0e64528b1f4c2
DAVID paredes <augustspies22@gmail.com>
2021-06-11 09:44:44 -0700

    Problem Seven Read

--- Problems.cs
+++ Problems.cs
@@ -22,7 +22,7 @@ namespace DatabaseFirstLINQ
             ProblemFour();
             ProblemFive();
             ProblemSix();
-            //ProblemSeven();
+            ProblemSeven();
             //ProblemEight();
             //ProblemNine();
             //ProblemTen();
@@ -131,16 +131,16 @@ namespace DatabaseFirstLINQ
 
         //// <><><><><><><><> R Actions (Read) with Foreign Keys <><><><><><><><><>
 
-        //private void ProblemSeven()
-        //{
-        //    // Write a LINQ query that retreives all of the users who are assigned to the role of Customer.
-        //    // Then print the users email and role name to the console.
-        //    var customerUsers = _context.UserRoles.Include(ur => ur.Role).Include(ur => ur.User).Where(ur => ur.Role.RoleName == "Customer");
-        //    foreach (UserRole userRole in customerUsers)
-        //    {
-        //        Console.WriteLine($"Email: {userRole.User.Email} Role: {userRole.Role.RoleName}");
-        //    }
-        //}
+        private void ProblemSeven()
+        {
+            // Write a LINQ query that retreives all of the users who are assigned to the role of Customer.
+            // Then print the users email and role name to the console.
+            var customerUsers = _context.UserRoles.Include(ur => ur.Role).Include(ur => ur.User).Where(ur => ur.Role.RoleName == "Customer");
+            foreach (UserRole userRole in customerUsers)
+            {
+                Console.WriteLine($"Email: {userRole.User.Email} Role: {userRole.Role.RoleName}");
+            }
+        }
 
         //private void ProblemEight()
         //{
